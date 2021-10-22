2b143ce929e4d29140c8e15f9663beead87d7b16
deborah <deborahregs@gmail.com>
2021-10-22 15:41:13 -0300

    ok

--- trabalho_final_modulo2/src/deborah/dbc/model/Motoboy.java
+++ trabalho_final_modulo2/src/deborah/dbc/model/Motoboy.java
@@ -7,39 +7,12 @@ public class Motoboy extends Funcionario {
         super(id, nome, salario);
     }
 
-    public Motoboy(int id, String nome, double salario, String placaMoto) {
-        super(id, nome, salario);
-        this.placaMoto = placaMoto;
-    }
-
-
-    public String getPlacaMoto() {
-        return placaMoto;
-    }
-
-    public void setPlacaMoto(String placaMoto) {
-        this.placaMoto = placaMoto;
-    }
 
 
     @Override
     public String toString() {
-        return "Motoboy";
-    }
-
-    @Override
-    public int getFuncionario() {
-        return 0;
+        return super.toString()+ "Motoboy";
     }
 
 
-    @Override
-    public void setIdFuncionario(Integer proximoId) {
-
-    }
-
-    @Override
-    public void setIdFuncionarios(Integer proximoId) {
-
-    }
 }

fcf940d82c88f91214f9c8e962261da5a99409ca
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-11-03 20:58:55 -0300

    "contato repositoy, service e entity"

--- Restaurante-api/Restaurante-api/src/main/java/com/dbc/trabalho_modulo_3/Restauranteapi/entity/ContatoEntity.java
+++ Restaurante-api/Restaurante-api/src/main/java/com/dbc/trabalho_modulo_3/Restauranteapi/entity/ContatoEntity.java
@@ -1,79 +1,22 @@
 package com.dbc.trabalho_modulo_3.Restauranteapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
     public class ContatoEntity {
-        private deborah.dbc.model.Cliente pessoa;
+        private Integer idCliente;
         private Integer idContato;
-        private String descricao;
-        private String telefone;
         private TipoContato tipo;
+        private String telefone;
+        private String descricao;
 
-        public ContatoEntity(String descricao, String telefone, TipoContato tipo) {
-            this.descricao = descricao;
-            this.telefone = telefone;
-            this.tipo = tipo;
-        }
 
-        public ContatoEntity() {
-        }
-
-        public deborah.dbc.model.Cliente getCliente() {
-            return pessoa;
-        }
-
-        public void setCliente(deborah.dbc.model.Cliente pessoa) {
-            this.pessoa = pessoa;
-        }
-
-        public ContatoEntity(Integer idContato, String descricao, String telefone, TipoContato tipo) {
-            this.idContato = idContato;
-            this.descricao = descricao;
-            this.telefone = telefone;
-            this.tipo = tipo;
-        }
-
-        public String getDescricao() {
-            return descricao;
-        }
-
-        public void setDescricao(String descricao) {
-            this.descricao = descricao;
-        }
-
-        public String getTelefone() {
-            return telefone;
-        }
-
-        public void setTelefone(String telefone) {
-            this.telefone = telefone;
-        }
-
-        public TipoContato getTipo() {
-            return tipo;
-        }
-
-        public void setTipo(TipoContato tipo) {
-            this.tipo = tipo;
-        }
-
-        public Integer getIdContato() {
-            return idContato;
-        }
-
-        public void setIdContato(Integer idContato) {
-            this.idContato = idContato;
-        }
-
-        @Override
-        public String toString () {
-            String tipoContato = "";
-            if (tipo.getTipo() == 1) {
-                tipoContato = "Residencial";
-            }
-            if (tipo.getTipo() == 2) {
-                tipoContato = "Comercial";
-            }
-            return "ID DO CONTATO: "+getIdContato()+"\nID do cliente: "+ getCliente().getIdCliente()+"\nNome do cliente: "+getCliente().getNome()+"\nDescrição do contato: " + descricao + "\nTelefone: " +  telefone + "\nTipo: " + tipoContato + "\n";
-        }
     }
 
 
